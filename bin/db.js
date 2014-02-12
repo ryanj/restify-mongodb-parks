c0bef02bfa7c6d3c361566cbebb3b1cec4e20e32
ryanj <ryan.jarvinen@gmail.com>
2014-02-11 17:01:09 -0800

    closing out db connections after bootstrapping

--- bin/db.js
+++ bin/db.js
@@ -2,37 +2,39 @@ var config      = require('config'),
     mongojs     = require('mongojs');
 
 var db_config   = config.db_config,
-    table_name  = config.table_name;
-var db = mongojs(db_config + table_name, [table_name] );
+    collection_name = config.collection_name;
+var db = mongojs(db_config + collection_name, [collection_name] );
 
 function init_db(){
-  var points = require('../parkcoord.json');
-  var error_response = "data already exists - bypassing db initialization step\n";
-  db[table_name].ensureIndex({'pos':"2d"}, function(err, doc){
+  var points = require(__dirname + '/../parkcoord.json');
+  db[collection_name].ensureIndex({'pos':"2d"}, function(err, doc){
     if(err){
       console.log(err);
-      console.log(error_response);
+      return db.close();
     }else{
       console.log("index added on 'pos'");
+      db[collection_name].count(function(errr, count){
+        if(errr){
+          console.log(errr);
+        }else if(count > 0){
+          console.log("data already exists - bypassing db initialization work...");
+        }else{
+          console.log("Importing map points...");
+          db[collection_name].insert(points);
+        }
+        return db.close();
+      });
     }
-    db[table_name].insert(points, function(errr, docs){
-      if(errr){
-        console.log(errr);
-      }else{
-        console.log("map points added");
-      }
-      return;
-    });
   });
 } 
 
 function flush_db(){
   console.log("Dropping the DB...");
-  db[table_name].drop(function(err){
+  db[collection_name].drop(function(err){
     if(err){
       console.log(err);
     }
-    return;
+    return db.close();
   });
 } 
 
@@ -53,19 +55,18 @@ function select_box(req, res, next){
     res.send(500, {http_status:400,error_msg: "this endpoint requires two pair of lat, long coordinates: lat1 lon1 lat2 lon2\na query 'limit' parameter can be optionally specified as well."});
     return console.error('could not connect to the database', err);
   }
-  db[table_name].find( {"pos" : {'$within': { '$box': [[lon1,lat1],[lon2,lat2]]}}}).limit(limit).toArray(function(err,rows){
+  db[collection_name].find( {"pos" : {'$within': { '$box': [[lon1,lat1],[lon2,lat2]]}}}).limit(limit).toArray(function(err,rows){
     if(err) {
       res.send(500, {http_status:500,error_msg: err})
       return console.error('error running query', err);
     }
-    //res.header("Content-Type:","application/json");
     res.send(rows);
     return rows;
   });
 };
 function select_all(req, res, next){
   console.log(db);
-  db[table_name].find(function(err, rows){
+  db[collection_name].find(function(err, rows){
     if(err) {
       res.send(500, {http_status:500,error_msg: err})
       return console.error('error running query', err);
